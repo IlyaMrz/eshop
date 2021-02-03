ba74aafbbace2be29687404f3a76b5b31e02addc
morozov ilya <knpkaa@gmail.com>
2021-02-03 18:44:53 +0300

    header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import HomePage from './Pages/homepage';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './Pages/shop';
+import Header from './Components/header/header';
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
