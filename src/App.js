61c36f6547cb4af08836be78662b515db3b233d1
morozov ilya <knpkaa@gmail.com>
2021-02-05 20:22:17 +0300

    firebase auth DB with state changing to user

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import ShopPage from './Pages/shop';
 import Header from './Components/header/header';
 import SignInUp from './Pages/SignInUp';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -19,9 +19,24 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = createUserProfileDocument(userAuth);
+
+        (await userRef).onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }
+          // , () =>{
+          //   console.log(this.state);
+          // }
+          );
+        });
+      }
+      this.setState({currentUser: userAuth})
     })
   }
 
