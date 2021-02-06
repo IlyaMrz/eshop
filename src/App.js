6f10d715e07b911c8820278663b78910119ea2b1
morozov ilya <knpkaa@gmail.com>
2021-02-06 18:33:50 +0300

    firebase auth with email and password

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ class App extends React.Component {
           //   console.log(this.state);
           // }
           );
+          console.log(this.state)
         });
       }
       this.setState({currentUser: userAuth})
--- src/Pages/SignInUp.jsx
+++ src/Pages/SignInUp.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SignIn from '../Components/sign-in/sign-in';
+import SignUp from '../Components/sign-up/sign-up';
 import './SignInUp.scss';
 
 const SignInUp = () => (
     <div className='signinup'>
         <SignIn />
+        <SignUp />
     </div>
 )
 
