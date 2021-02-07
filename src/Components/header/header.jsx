a1b26388db2bf99b2cd33562656f8d02bc0791e9
morozov ilya <knpkaa@gmail.com>
2021-02-07 21:40:48 +0300

    added CartIcon,CartDropdown components. implemented redux state to toggle cart visibility

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -4,8 +4,11 @@ import {Link} from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon';
+import CartDowndrop from '../cart-dropdown/cart-dropdown';
 
-const Header =({ currentUser }) => (
+
+const Header =({ currentUser, hidden }) => (
     <div className="header">
         <Link className="logo-container" to="/">
             <Logo className="logo" />
@@ -19,14 +22,15 @@ const Header =({ currentUser }) => (
                 :
                 <Link className="option" to='/signin'>SIGN IN</Link>
             }
+         <CartIcon />
         </div>
-        
-            
+        { hidden ? null : <CartDowndrop />}
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
