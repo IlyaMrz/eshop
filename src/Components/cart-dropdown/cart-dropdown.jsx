7eefa79f640be2449975857bf1044db78b3c6874
morozov ilya <knpkaa@gmail.com>
2021-02-08 18:29:14 +0300

    cart items pulls from state and shows in dropdown cart

--- src/Components/cart-dropdown/cart-dropdown.jsx
+++ src/Components/cart-dropdown/cart-dropdown.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import CartItem from '../cart-item/cart-item';
 import CustomButton from '../custom-button/custom-button';
 import './cart-dropdown.scss';
+import { connect } from 'react-redux';
 
-const CartDowndrop = () => (
+const CartDowndrop = ({cartItems}) => (
     <div className='cart-dropdown'>
-        <div className='cart-items' />
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+        <div className='cart-items'>
+            {
+                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
+            }
+            <CustomButton>GO TO CHECKOUT</CustomButton>
+        </div>
     </div>
 )
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems
+})
 
-
-export default CartDowndrop;
+export default connect(mapStateToProps, null)(CartDowndrop);
