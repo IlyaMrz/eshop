b93ca9108efe13f1d687d8062e2387c55e71f2ca
morozov ilya <knpkaa@gmail.com>
2021-03-02 00:03:43 +0300

    config hidden

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,16 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-
-const config = {
-    apiKey: "AIzaSyDLpBSJQ0vVwkV0tu0g0ocEeXEPy-PxxI8",
-    authDomain: "eshop-d706b.firebaseapp.com",
-    projectId: "eshop-d706b",
-    storageBucket: "eshop-d706b.appspot.com",
-    messagingSenderId: "285748473475",
-    appId: "1:285748473475:web:f36ad7c25c67a4725635b6",
-    measurementId: "G-EGZD0ZGZJR"
-};
+import config from './config';
 
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
