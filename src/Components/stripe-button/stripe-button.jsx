8872c8164f45894175fd632fe714acab17dd4d30
morozov ilya <knpkaa@gmail.com>
2021-04-13 21:15:02 +0300

    arrow and stripe token commented

--- src/Components/stripe-button/stripe-button.jsx
+++ src/Components/stripe-button/stripe-button.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({price}) =>{
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_e8TShNv4bqnar9lokQETbn8T';
+    // const onToken = token => {
+    //     axios({
+    //         url: 'payment',
+    //         method: 'post',
+    //         data: {
+    //             amount: priceForStripe,
+    //             token
+    //         }
+    //     }).then(response => {
+    //         alert('Payment succesful')
+    //     }).catch(error => {
+    //         console.log("Payment error:", JSON.parse(error));
+    //         alert('There was an issue with your payment. Please sure you use the provided credit.')
+    //     })
+    // }
     const onToken = token => {
         console.log(token);
         alert('Payment Successful');
