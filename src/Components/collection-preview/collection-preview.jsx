c09c1446e15e1190dedec5e7730190126596c49e
morozov ilya <knpkaa@gmail.com>
2021-04-24 00:14:40 +0300

    collection preview 4 items in desctop view 2 in mobile

--- src/Components/collection-preview/collection-preview.jsx
+++ src/Components/collection-preview/collection-preview.jsx
@@ -3,19 +3,34 @@ import { withRouter } from 'react-router-dom';
 import CollectionItem from '../collection-item/collection-item';
 import { CollectionPreviewContainer, PreviewContainer, TitleContainer } from './collection-preview.styles';
 
-const CollectionPreview = ({title, items, history, match, routeName}) => (
+const CollectionPreview = ({title, items, history, match, routeName}) =>{
+    const [width, setWidth] = React.useState(window.innerWidth);
+    const breakPoint = 730;
+    React.useEffect(() => {
+        const handleWindowResize = () => setWidth(window.innerWidth);
+        window.addEventListener("resize", handleWindowResize);
+     
+         
+        return () => window.removeEventListener("resize", handleWindowResize);
+       },[]);
+return (
     <CollectionPreviewContainer>
         <TitleContainer onClick={() => history.push(`${match.path}/${routeName}`)}>
             {title.toUpperCase()+" ➥"}
         </TitleContainer>
         <PreviewContainer>
-            {items
+            { width > breakPoint ? items
             .filter((item, idx) => idx < 4)
             .map((item) => (
                 <CollectionItem key={item.id} item={item}/>
-             ))}
+             )) : items
+             .filter((item, idx) => idx < 2)
+             .map((item) => (
+                 <CollectionItem key={item.id} item={item}/>
+              ))
+            }
         </PreviewContainer>
     </CollectionPreviewContainer>
-)
+)}
 
 export default withRouter(CollectionPreview);
